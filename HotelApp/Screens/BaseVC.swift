2c68e239dbebed22323b4012673dcf46d69d5d6b
Nachaded <btstheorys@gmail.com>
2023-09-06 17:57:13 +0300

    add some fix

--- HotelApp/Screens/BaseVC.swift
+++ HotelApp/Screens/BaseVC.swift
@@ -15,10 +15,20 @@ class BaseVC: UITableViewController {
     
     var navigationBarTitle: String? { nil }
     
+    private func setupLabel(text: String?) {
+        let titleLabel = UILabel()
+        titleLabel.text = text
+        titleLabel.numberOfLines = 5
+        titleLabel.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        titleLabel.textAlignment = .center
+        titleLabel.sizeToFit()
+        navigationItem.titleView = titleLabel
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.setNavigationBarHidden(isNavigationBarHidden, animated: false)
-        title = navigationBarTitle
+        setupLabel(text: navigationBarTitle)
     }
     
     func showAlert(title: String, message: String) {
